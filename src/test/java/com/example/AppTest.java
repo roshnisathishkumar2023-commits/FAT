6509418dcace806b8e9b713fc431483ed6e55229
Roshni Sathishkumar <roshni.sathishkumar2023@vitstudent.ac.in>
2026-04-16 14:24:51 +0530

    Create AppTest.java

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,27 @@
+package com.library.app;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+
+public class AppTest 
+{
+    
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+   
+    @Test
+    public void testFineCalculationLogic() {
+        int allowedDays = 14;
+        int borrowedDays = 20;
+        int extraDays = Math.max(0, borrowedDays - allowedDays);
+        double finePerDay = 2.0;
+        double totalFine = extraDays * finePerDay;
+        
+        assertTrue("Fine should be 12.0 for 6 extra days", totalFine == 12.0);
+    }
+}
